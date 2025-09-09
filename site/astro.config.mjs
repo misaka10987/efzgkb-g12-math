0dadbd250293155a553997cfd1dd607eada76eb9
misaka10987 <misaka10987@outlook.com>
2025-09-09 22:15:42 +0800

    deploy to github pages

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,6 +7,8 @@ import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://misaka10987.github.io',
+  base: '/site-efzgkb-g12-math',
   trailingSlash: 'always',
   vite: {
     resolve: {
